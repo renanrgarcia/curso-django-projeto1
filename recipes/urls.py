2835487d0b817d0ecbac0b09995b66265512c191
Renan Garcia <renanrgarcia@gmail.com>
2024-02-12 07:38:37 -0300

    Nome único nas URLs e app_name para Namespace

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+# recipes:recipe
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home, name="recipes-home"),  # Home
-    path('recipes/<int:id>/', views.recipe, name="recipes-recipe"),
+    path('', views.home, name="home"),  # Home
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
