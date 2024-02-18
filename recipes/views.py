e0cc640d09407edd37426d23daae6162335befe7
Renan Garcia <renanrgarcia@gmail.com>
2024-02-18 11:15:16 -0300

    Mostrando a página de detalhes da receita

--- recipes/views.py
+++ recipes/views.py
@@ -12,16 +12,9 @@ def home(request):
 
 
 def category(request, category_id):
-    # recipes = Recipe.objects.filter(
-    #     category__id=category_id,
-    #     is_published=True
-    # ).order_by('-id')
-
-    # if not recipes:
-    #     raise Http404('Not found 😢')
     recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id,
-        is_published=True
+        is_published=True,
     ).order_by('-id'))
 
     return render(request, 'recipes/pages/category.html', context={
@@ -31,7 +24,11 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
